809900f69663077b083ecb3aaed021b0cb39f648
Damien Pla <damien.pla@live.fr>
2025-11-06 13:24:22 +0100

    Ajout fonction Ajout/maj/supp

--- write/manip.py
+++ write/manip.py
@@ -1,8 +1,9 @@
 from models.Movie import Movie
 from exeptions.InvalidAgeLimitException import InvalidAgeLimitException
 import csv
+import os
 
-
+list_film = [] 
 def menu():
     print("Que voulez vous faire : ")
     print("1. Ajouter")
@@ -19,21 +20,68 @@ def saisie_film():
     annee_production = input("Année : ")
     genre = input("Genre : ")
     age_limite = input("Age limite : ")
-    verif_age(age_limite)
-    film = Movie(Movie.id,titre,annee_production,genre,age_limite)
+    id2 = int(Movie.recup_id(read_csv()))
+    id2 +=1
+    film = Movie(id2,titre,annee_production,genre,age_limite)
     return film
-
-def verif_age(age):
-    if age.isalnum() == False:
-        raise InvalidAgeLimitException()
-
-
+   
 def write_csv(film:Movie):
-    fichier = open("data/movies.csv","wt",newline=";")
-    filmCSV = csv.writer(fichier,delimiter=";")
-    filmCSV.writerow([film.titre,film.annee_production,film.genre,film.age_limite])
+    fichier = open("data/movies.csv", "at")
+    filmCSV = csv.writer(fichier,delimiter=",")
+    filmCSV.writerow([film.id,film.titre,film.annee_production,film.genre,film.age_limite])
     fichier.close()
 
+def afficher_film():
+    list_film = read_csv()
+    for film in list_film:
+        print(film.id,film.titre,film.annee_production,film.genre,film.age_limite)
+    print("Quel film voulez vous selectionner ? ")
+    choix = int(input(print("Tapez son id : ")))
+    return choix
+    
+def read_csv():
+    list_film = []
+    fichier = open("data/movies.csv", "r")
+    filmCSV = csv.reader(fichier,delimiter=",")
+    for line in filmCSV:
+        if not line:  # ligne vide
+            continue
+        list_film.append(Movie(line[0],line[1],line[2],line[3],line[4]))
+    fichier.close()
+    return list_film        
+
+
+def menu_update_film(id:int):
+    list_film = []
+    list_film = read_csv()
+    print("Quelle information souhaité vous modfier?")
+    choix = input(print("1.Titre","2.Année","3.Genre","4.Age"))
+    match choix:
+        case "1":
+            list_film[id].titre = input("Tapez le nouveau titre")
+        case "2":
+            list_film[id].Annee_production = input("Tapez la nouvelle année")   
+        case "3":
+            list_film[id].genre = input("Tapez le nouveau genre")
+        case "4":
+            list_film[id].age_limite = input("Tapez le nouveau age limite")
+    update_movie_csv(list_film)
+
+def update_movie_csv(list_film):
+    open("data/movies.csv", "w").close()
+    fichier = open("data/movies.csv", "a",newline='')
+    filmCSV = csv.writer(fichier,delimiter=",")
+    id = 1
+    for film in list_film:
+        filmCSV.writerow([id,film.titre,film.annee_production,film.genre,film.age_limite])
+        id +=1
+    fichier.close()
+
+def delete_movie():
+    list_film = read_csv()
+    choix =afficher_film()
+    list_film.pop(choix-1)
+    update_movie_csv(list_film)
 
 
 
@@ -51,6 +99,9 @@ while True :
             else:
                 print("Saisie OK : ")
         case "2":
-            pass
+            id = afficher_film()
+            menu_update_film(id)
+        case "3":
+            delete_movie()
         case "0":
             break
--- write/models/Movie.py
+++ write/models/Movie.py
@@ -3,14 +3,15 @@ class Movie() :
     def __init__(self,id:int,titre:str,anne_production:int,genre:str,age_limite:int):
         self.id = id 
         self.titre = titre
-        self.annee_prodution = anne_production
+        self.annee_production = anne_production
         self.genre = genre
         self.age_limite = age_limite
-        id = id + 1
+
 
     def __str__(self):
         print(f"le film {self.titre}est du genre {self.genre} et est sortie en {self.annee_prodution}. Il faut avoir {self.age_limite} pour le voir")
     
     @classmethod
-    def recup_id():
-        return id 
+    def recup_id(self,listfilm:list):
+        for film in listfilm[::-1]:
+            return film.id
