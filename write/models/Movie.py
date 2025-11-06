8b1f0b45922e3f5cefb3242774a0aeaf17aca368
Damien Pla <damien.pla@live.fr>
2025-11-06 09:21:24 +0100

    creation de read/write/movie.py

---
+++ write/models/Movie.py
@@ -0,0 +1,10 @@
+class Movie() :
+    def __init__(self,id:int,titre:str,anne_production:int,genre:str,age_limite:int):
+        self.id = id 
+        self.titre = titre
+        self.annee_prodution = anne_production
+        self.genre = genre
+        self.age_limite = age_limite
+
+    def __str__(self):
+        print(f"le film {self.titre}est du genre {self.genre} et est sortie en {self.annee_prodution}. Il faut avoir {self.age_limite} pour le voir")
